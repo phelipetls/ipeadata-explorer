e75d5be90743148c71ee5fb358a6f79a6260057b
phelipe <phelipe_teles@hotmail.com>
2020-09-02 23:15:27 -0300

    Make NavigationBar more responsive

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,31 +1,70 @@
-import React from "react";
+import React, { useState } from "react";
 
-import { AppBar, Toolbar, Link, Typography } from "@material-ui/core";
-import { makeStyles } from '@material-ui/core/styles';
+import {
+  AppBar,
+  Toolbar,
+  Link,
+  Typography,
+  Hidden,
+  Menu,
+  MenuItem,
+  IconButton
+} from "@material-ui/core";
 
-const useStyles = makeStyles((theme) => ({
+import { MoreVert } from "@material-ui/icons";
+
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles(theme => ({
   toolbar: {
+    height: "4em",
     justifyContent: "space-between"
   },
   link: {
     margin: "1em",
+    textDecoration: "none",
+    "&:hover": {
+    }
   }
-}))
+}));
 
 export default function NavigationBar() {
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  const useMenuOpen = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const useMenuClose = () => {
+    setAnchorEl(null);
+  };
+
   const classes = useStyles();
+  const links = [
+    <Link key={1} className={classes.link}>Temas</Link>,
+    <Link key={2} className={classes.link}>Países</Link>,
+    <Link key={3} className={classes.link}>Níveis Geográficos</Link>
+  ];
 
   return (
-    <AppBar color="transparent">
+    <AppBar color="transparent" position="static">
       <Toolbar className={classes.toolbar}>
         <Typography variant="h5" color="primary">
           IPEA
         </Typography>
-        <nav>
-          <Link className={classes.link}>Temas</Link>
-          <Link className={classes.link}>Países</Link>
-          <Link className={classes.link}>Níveis Geográficos</Link>
-        </nav>
+        <Hidden xsDown>
+          <nav>{links}</nav>
+        </Hidden>
+        <Hidden smUp>
+          <IconButton onClick={useMenuOpen}>
+            <MoreVert />
+          </IconButton>
+          <Menu open={Boolean(anchorEl)} anchorEl={anchorEl} onClick={useMenuClose}>
+            {links.map((link, index) => {
+              return <MenuItem key={index}>{link}</MenuItem>;
+            })}
+          </Menu>
+        </Hidden>
       </Toolbar>
     </AppBar>
   );
