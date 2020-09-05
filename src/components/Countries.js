9e5ab44b22cafac83c757e5324b676a4f26014a7
phelipe <phelipe_teles@hotmail.com>
2020-09-05 11:21:09 -0300

    Implement table sort mechanism

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -7,7 +7,8 @@ import {
   TableHead,
   TableBody,
   TableRow,
-  TableCell
+  TableCell,
+  TableSortLabel
 } from "@material-ui/core";
 
 import { makeStyles } from "@material-ui/core/styles";
@@ -21,11 +22,28 @@ const useStyles = makeStyles(theme => ({
 }));
 
 const URL =
-  "http://ipeadata2-homologa.ipea.gov.br/api/v1/Paises?$expand=Metadados($select=SERCODIGO)";
+  "http://ipeadata2-homologa.ipea.gov.br/api/v1/Paises?$expand=Metadados($select=SERCODIGO;$count=true)";
+
+const invertSortDirections = {
+  asc: "desc",
+  desc: "asc"
+};
+
+const sortFunctions = {
+  string: (a, b) => a.localeCompare(b),
+  numeric: (a, b) => a - b,
+}
+
+const headers = {
+  PAINOME: { type: "string", label: "País" },
+  "Metadados@odata.count": { type: "numeric", label: "Qtd. de séries" }
+};
 
 export default function Countries() {
-  const [countries, setCountries] = useState([]);
   const classes = useStyles();
+  const [countries, setCountries] = useState([]);
+  const [order, setOrder] = useState("desc");
+  const [orderBy, setOrderBy] = useState("Metadados@odata.count");
 
   useEffect(() => {
     async function fetchCountries() {
@@ -35,23 +53,62 @@ export default function Countries() {
     }
 
     fetchCountries();
-  }, [])
+  }, []);
+
+  const handleSort = (e, header) => {
+    if (header === orderBy) {
+      setOrder(invertSortDirections[order]);
+    } else {
+      setOrder("desc");
+      setOrderBy(header);
+    }
+  };
+
+  const headerType = headers[orderBy].type;
+
+  const sortOrder = order === "asc" ? 1 : -1;
+  const sortFunction = sortFunctions[headerType];
+  const sorter = (a, b) => {
+    return sortOrder * sortFunction(a, b);
+  }
 
   return (
     <TableContainer component={Paper} className={classes.tableContainer}>
       <Table className={classes.table}>
         <TableHead>
           <TableRow>
-            <TableCell sortDirection="desc">País</TableCell>
-            <TableCell align="right">Qtd. de séries</TableCell>
+            {Object.entries(headers).map(([header, props]) => {
+              return (
+                <TableCell
+                  key={header}
+                  value={header}
+                  align={props.type === "numeric" ? "right" : "left"}
+                  sortDirection={orderBy === header ? order : false}
+                >
+                  <TableSortLabel
+                    active={orderBy === header}
+                    direction={orderBy === header ? order : "asc"}
+                    onClick={e => handleSort(e, header)}
+                  >
+                    {props.label}
+                  </TableSortLabel>
+                </TableCell>
+              );
+            })}
           </TableRow>
         </TableHead>
         <TableBody>
-          {countries.map(country => {
+          {countries
+              .sort((a, b) => sorter(a[orderBy], b[orderBy]))
+              .map(country => {
             return (
-              <TableRow key={country.PAICODIGO}>
-                <TableCell component="th" scope="row">{country.PAINOME}</TableCell>
-                <TableCell align="right">{country.Metadados.length}</TableCell>
+              <TableRow key={country["PAICODIGO"]}>
+                <TableCell component="th" scope="row">
+                  {country["PAINOME"]}
+                </TableCell>
+                <TableCell align="right">
+                  {country["Metadados@odata.count"]}
+                </TableCell>
               </TableRow>
             );
           })}
