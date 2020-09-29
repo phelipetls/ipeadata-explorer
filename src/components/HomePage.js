360db8ffc4a1f659ee7c9209fc0a1ed06205491e
phelipe <phelipe_teles@hotmail.com>
2020-09-28 21:22:45 -0300

    Add HomePageFeatures component

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,9 +1,10 @@
 import React from "react";
 
 import { Link as RouterLink } from "react-router-dom";
-import { Link, Grid, Paper, Typography } from "@material-ui/core";
-import { BarChart, Search } from "@material-ui/icons";
+import { Link, Typography } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
+
+import HomePageFeatures from "./HomePageFeatures";
 
 const useStyles = makeStyles(theme => ({
   siteTitle: {
@@ -12,23 +13,6 @@ const useStyles = makeStyles(theme => ({
   },
   siteDescription: {
     maxWidth: "100ch",
-  },
-  featuresSection: {
-    marginTop: theme.spacing(4),
-    marginBottom: theme.spacing(4),
-  },
-  feature: {
-    height: "100%",
-    paddingTop: theme.spacing(2),
-    paddingBottom: theme.spacing(2),
-  },
-  description: {
-    marginTop: theme.spacing(2),
-    textAlign: "center",
-    maxWidth: "70%",
-    flex: 1,
-    display: "flex",
-    alignItems: "center",
   },
 }));
 
@@ -62,45 +46,7 @@ export default function HomePage() {
         </Typography>
       </div>
 
-      <Grid
-        container
-        justify="center"
-        spacing={3}
-        className={classes.featuresSection}
-      >
-        <Grid item sm={4} xs={12} key="1">
-          <Grid
-            container
-            direction="column"
-            alignItems="center"
-            component={Paper}
-            className={classes.feature}
-          >
-            <Search fontSize="large" />
-
-            <Typography className={classes.description}>
-              Explore a base de dados com a ajuda de diversos filtros.
-            </Typography>
-          </Grid>
-        </Grid>
-
-        <Grid item sm={4} xs={12} key="2">
-          <Grid
-            container
-            direction="column"
-            alignItems="center"
-            component={Paper}
-            className={classes.feature}
-          >
-            <BarChart fontSize="large" />
-
-            <Typography className={classes.description}>
-              Visualize séries temporais numéricas, categóricas e com recortes
-              geográficos.
-            </Typography>
-          </Grid>
-        </Grid>
-      </Grid>
+      <HomePageFeatures />
     </>
   );
 }
