6077b79ef91650e9c14099a2c67d06717c30e163
phelipe <phelipe_teles@hotmail.com>
2020-10-03 15:21:01 -0300

    Minimizing start up time
    I followed the instructions here:
    https://material-ui.com/guides/minimizing-bundle-size/

--- src/styles/Theme.js
+++ src/styles/Theme.js
@@ -1,11 +1,11 @@
-import { colors } from "@material-ui/core";
+import { blue, green, red } from "@material-ui/core/colors";
 import { createMuiTheme } from "@material-ui/core/styles";
 
 export const theme = createMuiTheme({
   palette: {
-    macro: colors.blue[600],
-    regional: colors.green[600],
-    social: colors.red[600],
+    macro: blue[600],
+    regional: green[600],
+    social: red[600],
   },
   link: {
     textDecoration: "none",
