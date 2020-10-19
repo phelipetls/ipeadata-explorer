fcdb2e8961786555adb8daa6518d58dbf1ab947a
phelipe <phelipe_teles@hotmail.com>
2020-10-19 10:54:50 -0300

    Fix eslint warnings

--- src/components/TableRowsCollapsed.js
+++ src/components/TableRowsCollapsed.js
@@ -23,7 +23,7 @@ export default function TableRowsCollapsed(props) {
       </TableHead>
 
       <TableBody>
-        {isLoading || rows.length == 0
+        {isLoading || rows.length === 0
           ? skeleton
           : rows.map(row => (
               <TableRowCollapsed
