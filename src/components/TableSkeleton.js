bb6f17068c219f45a480a67574a384e9293a76c4
Phelipe Teles <phelipe_teles@hotmail.com>
2020-10-24 12:33:58 -0300

    Use Array.from to create array of given length

--- src/components/TableSkeleton.js
+++ src/components/TableSkeleton.js
@@ -4,17 +4,13 @@ import { Skeleton } from "@material-ui/lab";
 import { TableRow, TableCell } from "@material-ui/core";
 
 export default function TableSkeleton({ nRows, nColumns }) {
-  return Array(nRows)
-    .fill(null)
-    .map((_, index) => (
-      <TableRow key={index}>
-        {Array(nColumns)
-          .fill(null)
-          .map((_, index) => (
-            <TableCell key={index}>
-              <Skeleton animation={false} />
-            </TableCell>
-          ))}
-      </TableRow>
-    ));
+  return Array.from({ length: nRows }).map((_, index) => (
+    <TableRow key={index}>
+      {Array.from({ length: nColumns }).map((_, index) => (
+        <TableCell key={index}>
+          <Skeleton animation={false} />
+        </TableCell>
+      ))}
+    </TableRow>
+  ));
 }
