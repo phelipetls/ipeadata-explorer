60830dc7f217a0f445a3d7135e7e4b0ebf1a61a8
Phelipe Teles <phelipe_teles@hotmail.com>
2020-10-26 22:09:27 -0300

    Change how to format/handle dates
    First, I needed to handle irregular periodicities by not subtracting at
    all.
    
    Also, the functions to format the date into ISO changed and tries to
    ignore time-related stuff.

--- src/api/utils.js
+++ src/api/utils.js
@@ -1,15 +1,19 @@
-import { formatISO, sub, subQuarters, startOfQuarter } from "date-fns";
+import { sub, subQuarters, startOfQuarter } from "date-fns";
+
+function formatISO(date) {
+  return date.toISOString().slice(0, 10) + "T00:00:00Z";
+}
 
 export function formatDateFromDatePicker(dateStr) {
   if (!dateStr) return;
   const [day, month, year] = dateStr.split("/");
   const date = new Date(year, month - 1, day);
-  return formatISO(date, { representation: "complete" });
+  return formatISO(date);
 }
 
 export function subtractSeriesMaxDate({ metadata, offset }) {
   return subtractDateByPeriod({
-    date: metadata.SERMAXDATA,
+    isoDateStr: metadata.SERMAXDATA,
     period: metadata.PERNOME,
     offset: offset,
   });
@@ -24,15 +28,19 @@ const periodicities = {
   Decenal: { periodName: "years", periodAmount: 10 },
 };
 
-function subtractDateByPeriod({ date, period, offset }) {
-  let newDate = new Date(date);
+/* Subtract from a date a given number of periods.
+ * The output date must be in ISO format.
+ * Do not convert to local time, ignore time completely.
+ */
+function subtractDateByPeriod({ isoDateStr, period, offset }) {
+  let newDate = new Date(isoDateStr);
 
   if (period === "Trimestral") {
     newDate = subQuarters(startOfQuarter(newDate), offset);
-  } else {
+  } else if (period !== "Irregular") {
     const { periodName, periodAmount } = periodicities[period];
     newDate = sub(newDate, { [periodName]: offset * periodAmount });
   }
 
-  return formatISO(newDate, { representation: "complete" });
+  return formatISO(newDate);
 }
