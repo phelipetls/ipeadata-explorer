c70f847344464b7bd4dbf69183cf09139335549f
Phelipe Teles <phelipe_teles@hotmail.com>
2020-11-29 13:25:14 -0300

    Update test setup

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,6 +1,10 @@
 import "@testing-library/jest-dom/extend-expect";
+import { server } from "./test-utils/server.js";
 
-import { server } from "./mocks/server.js";
-
-beforeAll(() => server.listen());
+beforeAll(() =>
+  server.listen({
+    onUnhandledRequest: "error",
+  })
+);
+afterEach(() => server.resetHandlers());
 afterAll(() => server.close());
--- src/test-utils/test-utils.js
+++ src/test-utils/test-utils.js
@@ -1,16 +1,11 @@
 import React from "react";
-
 import { render } from "@testing-library/react";
-
 import { theme } from "../styles/Theme";
 import { ThemeProvider } from "@material-ui/core/styles";
-
-import DateFnsUtils from "@date-io/date-fns";
 import { MuiPickersUtilsProvider } from "@material-ui/pickers";
-
 import { MemoryRouter as Router } from "react-router-dom";
-
-import App from "../App";
+import DateFnsUtils from "@date-io/date-fns";
+import { readFileSync, existsSync } from "fs";
 
 const customRender = (component, { routerOptions, ...renderOptions } = {}) => {
   return render(
@@ -25,8 +20,15 @@ const customRender = (component, { routerOptions, ...renderOptions } = {}) => {
   );
 };
 
+export function readJson(path) {
+  if (!existsSync(path)) {
+    throw new Error(`${path} does not exist`);
+  }
+  const file = readFileSync(path, {
+    encoding: "utf-8",
+  });
+  return JSON.parse(file);
+}
+
 export * from "@testing-library/react";
-
 export { customRender as render };
-
-export { App };
