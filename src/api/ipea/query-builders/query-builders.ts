8bdd5d6ad2576ddf4d8a6c0af3b6cdcc31ce1ba9
Phelipe Teles <phelipe_teles@hotmail.com>
2021-06-12 18:06:54 -0300

    Do not format date differently if it is an end date

--- src/api/ipea/query-builders/query-builders.ts
+++ src/api/ipea/query-builders/query-builders.ts
@@ -40,7 +40,7 @@ export function getDateFilter({ start, end, lastN, metadata }: dateFilters) {
   if (start && end) {
     return limitByDate({
       start: formatDateToBackend(start),
-      end: formatDateToBackend(end, { isEndDate: true }),
+      end: formatDateToBackend(end),
     });
   }
 
@@ -49,7 +49,7 @@ export function getDateFilter({ start, end, lastN, metadata }: dateFilters) {
   }
 
   if (end) {
-    return limitByDate({ end: formatDateToBackend(end, { isEndDate: true }) });
+    return limitByDate({ end: formatDateToBackend(end) });
   }
 
   return limitByDate({
