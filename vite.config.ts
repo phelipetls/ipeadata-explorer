7702b1f94e6d6b44b24e505d4021fa0eed77cae7
Phelipe Teles <phelipe_teles@hotmail.com>
2021-06-20 15:02:33 -0300

    Revert commits migrating from CRA to Vite
    It didn't work well after wall because Rollup produced very large
    bundle sizes. It was not clear how I could improve it.
    
    Revert "Migrate from react-app to vite to start app"
    
    This reverts commit d4a7e7ef42f12543255cae7893ee768e20bd30bc.
    
    Revert "Run jest tests successfully without react-scripts"
    
    This reverts commit 84523bf53095888ae821b5751d8f99bf242fe87f.
    
    Revert "Remove CRA development setup dependencies"
    
    This reverts commit 834fe859a7889e92dd9d9ad6ae44df2ae994c0fc.
    
    Revert "Remove react-scripts and scripts using it from project"
    
    This reverts commit 8ebae30697708d4b51010c521c8079e1ffef452d.
    
    Revert "Add jest as dev dependency"
    
    This reverts commit decf22c23a9c259c17aeb65159a2aaf0e04e39f8.

--- vite.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
-
-export default defineConfig({
-  build: {
-    outDir: "build",
-  },
-  plugins: [tsconfigPaths()],
-});
