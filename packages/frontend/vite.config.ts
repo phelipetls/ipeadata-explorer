67db8bd9f0db59f8a27b23282810838cce681ae8
Phelipe Teles <phelipe_teles@hotmail.com>
2025-11-30 22:12:11 -0300

    Fix build broken to import.meta.env usage in vite.config.ts

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -1,15 +1,19 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-export default defineConfig({
-  base: import.meta.env.VITE_APP_BASENAME,
-  plugins: [
-    react({
-      babel: {
-        plugins: ['babel-plugin-react-compiler'],
-      },
-    }),
-    tailwindcss(),
-  ],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd())
+
+  return {
+    base: env['VITE_APP_BASENAME'],
+    plugins: [
+      react({
+        babel: {
+          plugins: ['babel-plugin-react-compiler'],
+        },
+      }),
+      tailwindcss(),
+    ],
+  }
 })
