88199efd0296814fd11cf936f8f48ecfd473209a
Phelipe Teles <phelipe_teles@hotmail.com>
2025-11-30 22:05:17 -0300

    Fix assets URL basename in Vite

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: import.meta.env.VITE_APP_BASENAME,
   plugins: [
     react({
       babel: {
