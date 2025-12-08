80563c71ac8c5594be25d33a8a2f32bbfac5a955
Phelipe Teles <phelipe_teles@hotmail.com>
2025-12-07 21:04:10 -0300

    Make Select style more consistent

--- packages/frontend/src/components/SelectButton.tsx
+++ packages/frontend/src/components/SelectButton.tsx
@@ -15,7 +15,7 @@ export function SelectButton({ render, children, className, ...rest }: Props) {
           'bg-surface-secondary',
           'rounded-full',
           'px-4 py-2 text-sm font-medium',
-          'border border-border-outline',
+          'shadow-sm',
           'hover:bg-surface-secondary-hover',
           'transition-colors',
           'disabled:cursor-not-allowed',
